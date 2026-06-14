dd294c093ca7201b54c4060eb13fb084fee43e67
Abhishek Aryan <abhishekaryan270@gmail.com>
2026-06-15 03:34:06 +0530

    Initial Build

---
+++ build.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd /d "D:\NetlifyProject"
+"C:\Program Files\nodejs\npm.cmd" run build
---
+++ dev.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd /d "D:\NetlifyProject"
+"C:\Program Files\nodejs\npm.cmd" run dev -- --host 0.0.0.0 --port 3000
